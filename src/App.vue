220c9914df22938cb654c14fba322222a33e97c0
Bastian Hodapp <bastianhodapp@gmail.com>
2020-02-07 21:31:38 +0100

    Added vue-typer for a cool typewriter effect

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/bassadinlogowithpaddingblackbg.svg"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <v-img
+                    alt="Vuetify Logo"
+                    class="shrink mr-2"
+                    contain
+                    src="@/assets/bassadinlogowithpaddingblackbg.svg"
+                    transition="scale-transition"
+                    width="40"
+                />
 
-        <h2>Bassadin</h2>
-      </div>
-    </v-app-bar>
+                <h2>Bassadin</h2>
+            </div>
+        </v-app-bar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
+    name: "App",
 
-  data: () => ({
-    //
-  })
+    data: () => ({
+        //
+    })
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Bassadin Webpage</h1>
-  </div>
+    <div class="home">
+        <v-container fluid>
+            <v-layout text-center column ma-4>
+                <h1 class="display-2 font-weight-bold mb-3">
+                    My name is Bastian. I do
+                    <vue-typer
+                        :text="[
+                            'Photography',
+                            'Programming',
+                            'Webdesign',
+                            'Gaming'
+                        ]"
+                        :shuffle="true"
+                        pre-type-delay="400"
+                    ></vue-typer
+                    >.
+                </h1>
+                <p class="subheading font-weight-regular">
+                    The location-based qr code network
+                </p>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
+import { VueTyper } from "vue-typer";
+
 export default {
-  name: "home"
+    name: "home",
+    components: {
+        VueTyper
+    }
 };
 </script>
