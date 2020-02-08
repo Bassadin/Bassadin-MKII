78fed2648abc66d9869d44a025327cb810f032b2
Bastian Hodapp <bastianhodapp@gmail.com>
2020-02-08 19:33:49 +0100

    Added divider and image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,30 @@
 <template>
     <div class="home">
-        <v-container fluid>
-            <v-layout text-center column ma-4>
-                <h1 class="display-2 font-weight-bold mb-3">
-                    My name is Bastian. I do
-                    <vue-typer
-                        :text="[
-                            'Photography',
-                            'Programming',
-                            'Webdesign',
-                            'Gaming'
-                        ]"
-                        :shuffle="true"
-                        pre-type-delay="400"
-                    ></vue-typer
-                    >.
-                </h1>
-                <p class="subheading font-weight-regular">
-                    The location-based qr code network
-                </p>
-            </v-layout>
-        </v-container>
+        <v-layout text-center column mt-8>
+            <h1 class="display-2 font-weight-bold mb-3">
+                My name is Bastian. I do
+                <vue-typer
+                    :text="[
+                        'Photography.',
+                        'Programming.',
+                        'Webdesign.',
+                        'Game Programming.'
+                    ]"
+                    :shuffle="true"
+                    :pre-type-delay="600"
+                    :pre-erase-delay="2500"
+                    :erase-delay="50"
+                    erase-style="backspace"
+                ></vue-typer>
+            </h1>
+            <v-divider></v-divider>
+            <v-flex text-center column mt-8>
+                <v-img
+                    src="@/assets/Bastian_white_blackBG.jpg"
+                    max-width="400px"
+                ></v-img>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
 
