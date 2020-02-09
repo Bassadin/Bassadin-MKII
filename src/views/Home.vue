158321254d359d4d47e460f7e71d995b323eb574
Bastian Hodapp <bastianhodapp@gmail.com>
2020-02-09 22:42:41 +0100

    Adjusted home vue some more

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,33 @@
 <template>
     <div class="home">
-        <v-row justify="center">
-            <h-layout text-center>
-                <h1 class="ma-5 text-center">
-                    My name is Bastian. I do
-                    <vue-typer
-                        :text="[
-                            'Photography.',
-                            'Programming.',
-                            'Webdesign.',
-                            'Game Programming.'
-                        ]"
-                        :shuffle="true"
-                        :pre-type-delay="600"
-                        :type-delay="80"
-                        :pre-erase-delay="2500"
-                        :erase-delay="50"
-                        erase-style="backspace"
-                    ></vue-typer>
-                </h1>
-                <v-flex class="justify-center">
-                    <v-img
-                        style="margin: 0 auto; border-radius: 50%; padding: 8px;"
-                        src="@/assets/Bastian_white_blackBG.jpg"
-                        width="100vw"
-                        max-width="350px"
-                    ></v-img>
-                </v-flex>
-            </h-layout>
-        </v-row>
+        <v-container fluid>
+            <h1 class="ma-5 text-center">
+                My name is Bastian. I do
+                <vue-typer
+                    :text="[
+                        'Photography.',
+                        'Programming.',
+                        'Webdesign.',
+                        'Game Programming.'
+                    ]"
+                    :shuffle="true"
+                    :pre-type-delay="600"
+                    :type-delay="80"
+                    :pre-erase-delay="2500"
+                    :erase-delay="50"
+                    erase-style="backspace"
+                ></vue-typer>
+            </h1>
+
+            <v-img
+                class="mx-auto"
+                style="border-radius: 50%; padding: 8px;"
+                src="@/assets/Bastian_white_blackBG.jpg"
+                width="100vw"
+                max-width="350px"
+            ></v-img>
+            <p class="text-center mt-6">{{ $t("message") }}</p>
+        </v-container>
     </div>
 </template>
 
