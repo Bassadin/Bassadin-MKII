884cfae7702575b66d9f4a791c86b66897bded9c
Bastian Hodapp <bastianhodapp@gmail.com>
2020-02-09 23:10:18 +0100

    Added German translation thingy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,9 @@
     <div class="home">
         <v-container fluid>
             <h1 class="ma-5 text-center">
-                My name is Bastian. I do
+                {{ $t("homepage.myNameIsIDo") }}
                 <vue-typer
-                    :text="[
-                        'Photography.',
-                        'Programming.',
-                        'Webdesign.',
-                        'Game Programming.'
-                    ]"
+                    :text="$t('homepage.iDoOptions')"
                     :shuffle="true"
                     :pre-type-delay="600"
                     :type-delay="80"
@@ -26,7 +21,6 @@
                 width="100vw"
                 max-width="350px"
             ></v-img>
-            <p class="text-center mt-6">{{ $t("message") }}</p>
         </v-container>
     </div>
 </template>
