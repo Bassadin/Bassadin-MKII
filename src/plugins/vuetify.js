5678f1be840b60d9a9b8b4ea5f72350ef95f1788
Bastian Hodapp <bastianhodapp@gmail.com>
2020-02-11 22:35:45 +0100

    changed color scheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
+import colors from "vuetify/lib/util/colors";
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.brown.darken4, // #E53935
+                secondary: colors.deepOrange.accent3, // #FFCDD2
+                accent: colors.yellow.accent3 // #3F51B5
+            }
+        }
+    }
+});
