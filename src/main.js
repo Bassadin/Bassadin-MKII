967dffbe817b3fa4d135f2562b7d9ef181d44533
Bastian Hodapp <bastianhodapp@gmail.com>
2020-12-03 14:34:12 +0100

    Add vue country flag

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
+import CountryFlag from "vue-country-flag";
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+Vue.component("country-flag", CountryFlag);
 
 new Vue({
     router,
