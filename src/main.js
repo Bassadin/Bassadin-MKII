c32014261f79ffeac3216d400a4b7c0c4ac51185
Bastian Hodapp <bastianhodapp@gmail.com>
2020-12-03 18:29:01 +0100

    Add vue-meta package

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
 import CountryFlag from "vue-country-flag";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 Vue.component("country-flag", CountryFlag);
+
+Vue.use(VueMeta);
 
 new Vue({
     router,
