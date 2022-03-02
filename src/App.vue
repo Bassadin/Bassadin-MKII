2a292de0902d3265236a1f69196fc826fa53a62e
Bastian Hodapp <bastianhodapp@gmail.com>
2022-03-02 10:57:37 +0100

    remove old comments

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,6 @@
             </router-link>
             <h2>Bassadin</h2>
             <v-spacer></v-spacer>
-            <!-- <v-btn color="lime darken-3" to="/about"
-                ><v-icon left>mdi-account</v-icon> About</v-btn
-            >
-            <v-btn color="light-blue accent-3" class="ml-3"
-                ><v-icon left>mdi-camera</v-icon> Photography</v-btn
-            >
-            <v-btn color="pink accent-3" class="ml-3"
-                ><v-icon left>mdi-head-lightbulb</v-icon> Projects</v-btn
-            > -->
         </v-app-bar>
 
         <router-view class="main-content" />
