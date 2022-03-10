ea9cd8d96821e50d9a4cf81e6791e48a10b57720
Bastian Hodapp <bastianhodapp@gmail.com>
2022-03-10 16:10:18 +0100

    main.js formatting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import './index.scss'
+import "./index.scss";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
