1bf872758dbedab4f543654334b839c2d1eed39a
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-06 12:52:20 +0200

    move everything into the app component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,58 @@
 <template>
-    <div id="super-main">
-        
+    <div class="super-main">
+        <Header />
 
-        <Main></Main>
+        <div class="hero-image">
+            <img src="@/assets/Bastian_white_blackBG.jpg" />
+            <h1>Bastian Hodapp</h1>
+        </div>
+
+        <SocialIcons />
     </div>
 </template>
 
 <script lang="ts">
-import Main from "@/views/Main.vue";
+import Header from "@/components/Header.vue";
+import SocialIcons from "./components/SocialIcons.vue";
 
 export default {
     name: "App",
-    components: {
-        Main,
+    components: { Header, SocialIcons },
+    metaInfo: {
+        title: "Bastian Hodapp",
     },
 };
 </script>
 
 <style lang="scss" scoped>
+.super-main {
+    min-height: 100vh;
+}
 
+.hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+
+    margin-top: 3rem;
+    flex-direction: column;
+    width: 100vw;
+
+    @include breakpoint("m") {
+        flex-direction: row;
+        width: 60vw;
+        margin-top: 15rem;
+    }
+
+    h1 {
+        color: white;
+        font-size: 4em;
+    }
+
+    img {
+        border-radius: 50%;
+        height: 40vh;
+    }
+}
 </style>
