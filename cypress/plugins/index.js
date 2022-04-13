28700e6506956a075a865f8db8d890f2373502c3
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-13 14:50:29 +0200

    formatting

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,15 +1,15 @@
-const path = require('path')
-const { startDevServer } = require('@cypress/vite-dev-server')
+const path = require("path");
+const { startDevServer } = require("@cypress/vite-dev-server");
 
 module.exports = (on, config) => {
-  on('dev-server:start', (options) => {
-    return startDevServer({
-      options,
-      viteConfig: {
-        configFile: path.resolve(__dirname, '../../vite.config.ts'),
-      },
-    })
-  })
+    on("dev-server:start", (options) => {
+        return startDevServer({
+            options,
+            viteConfig: {
+                configFile: path.resolve(__dirname, "../../vite.config.ts"),
+            },
+        });
+    });
 
-  return config
-}
+    return config;
+};
