0843f62aad522dac5a06b03920959ea2a3de07a8
Bastian Hodapp <bastianhodapp@gmail.com>
2022-07-27 22:48:12 +0200

    formatting

--- src/App.spec.ct.ts
+++ src/App.spec.ct.ts
@@ -5,10 +5,6 @@ import App from "@/App.vue";
 
 describe("basic main page tests", () => {
     beforeEach(() => {
-        // Cypress starts out with a blank slate for each test
-        // so we must tell it to visit our website with the `cy.visit()` command.
-        // Since we want to visit the same URL at the start of all our tests,
-        // we include it in our beforeEach function so that it runs before each test
         mount(App);
     });
 
