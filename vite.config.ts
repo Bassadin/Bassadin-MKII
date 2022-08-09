f48ab49de5092f1a82b2aebf2aedc81bf44bab90
Bastian Hodapp <bastianhodapp@gmail.com>
2022-08-10 00:30:14 +0200

    fix vite components alias

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
-            "@/components": path.resolve(__dirname, "./src/components"),
+            "@components": path.resolve(__dirname, "./src/components"),
         },
     },
     css: {
