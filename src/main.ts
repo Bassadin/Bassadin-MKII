eb6bdb064c5557fbe43d555ce001aeda8066d7c2
Bastian Hodapp <bastianhodapp@gmail.com>
2022-08-13 13:53:51 +0200

    restructure main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-import { createApp } from "vue";
-import { createMetaManager, plugin as vueMetaPlugin } from "vue-meta";
-import mdiVue from "mdi-vue/v3";
-import * as mdijs from "@mdi/js";
-
 import "@/scss/index.scss";
 
+import { createApp } from "vue";
 import App from "@/App.vue";
-
 const app = createApp(App);
 
 // Load vue-meta plugin
+import { createMetaManager, plugin as vueMetaPlugin } from "vue-meta";
 const metaManager = createMetaManager();
 app.use(metaManager);
 app.use(vueMetaPlugin); // optional, only needed for OptionsAPI (see below)
 
+// MDI Icons
+import * as mdijs from "@mdi/js";
+import mdiVue from "mdi-vue/v3";
 app.use(mdiVue, {
     icons: mdijs,
 });
