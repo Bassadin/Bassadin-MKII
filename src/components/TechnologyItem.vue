6eeae43dc2b239dc42238f4b84e9868653bf2e84
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-20 13:21:16 +0100

    more windi conversion

--- src/components/TechnologyItem.vue
+++ src/components/TechnologyItem.vue
@@ -2,10 +2,10 @@
     <a
         :href="url"
         target="_blank"
-        class="technology-item transition-colors duration-200 p-4 flex flex-row items-center rounded-lg"
+        class="technology-item transition-colors duration-200 p-4 flex flex-row items-center rounded-lg basis-full"
     >
         <mdicon class="technology-item--icon" :name="iconName" size="45" />
-        <h3 class="technology-item--title">
+        <h3 class="ml-2">
             <slot name="title"></slot>
         </h3>
     </a>
@@ -32,14 +32,8 @@ defineProps({
         background-color: $card-background-color-hover;
     }
 
-    flex: 0 0 100%;
-
     @include breakpoint("s") {
         flex: 0 0 calc(33% - 0.6rem);
-    }
-
-    h3 {
-        margin: 0 0 0 0.6rem;
     }
 }
 </style>
