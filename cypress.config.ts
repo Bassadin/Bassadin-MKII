fddcbb76954538a06ed15f19ce395e61c89f2e3e
Bastian Hodapp <bastianhodapp@gmail.com>
2023-12-08 19:22:43 +0100

    chore: update deps and remove cypress

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-    e2e: {
-        baseUrl: "http://localhost:3000",
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        setupNodeEvents(on, config) {},
-    },
-});
