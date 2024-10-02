8153d1cbcba8fbcf872579b8394a881fd5bf0f89
Bastian Hodapp <bastianhodapp@gmail.com>
2024-10-02 22:21:36 +0200

    fix: adjust base bg and text colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="grid min-h-max grid-cols-1 grid-rows-[auto_1fr_auto] bg-stone-50 font-sans text-stone-900 dark:bg-stone-800 dark:text-stone-100"
+        class="grid min-h-max grid-cols-1 grid-rows-[auto_1fr_auto] bg-stone-100 font-sans text-stone-800 dark:bg-stone-800 dark:text-stone-200"
     >
         <SectionsSiteHeader />
         <div class="m-1 break-words p-2 lg:m-4 lg:mx-auto lg:w-1/2">
